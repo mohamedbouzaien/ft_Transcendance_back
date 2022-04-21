13ccac5a1ae7c74ca930249b392acc84e29c1ef8
Mohamed Bouzaien <mbouzaie@student.42.fr>
2022-04-21 17:32:18 +0200

    add global prefix /api

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.setGlobalPrefix('api');
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
   app.use(cookieParser());
   await app.listen(3000);
