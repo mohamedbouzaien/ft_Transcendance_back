0cc5ccc806324911f838a6672ca6096f51e7b9d0
AdrienCastelbou <acastelbou1@gmail.com>
2022-04-29 15:23:03 +0200

    put in available channels only non joined chans

--- src/chat/services/chat.service.ts
+++ src/chat/services/chat.service.ts
@@ -124,7 +124,7 @@ export class ChatService {
     const user_channels = await this.exctractAllChannelsForUser(user);
     const accessible_channels = await this.channelsService.getAllAccessibleChannels();
     const channels_ids = new Set(user_channels.map(channel => channel.id));
-    const available_channels = [...user_channels, ...accessible_channels.filter(channel => !channels_ids.has(channel.id))];
+    const available_channels = [...accessible_channels.filter(channel => !channels_ids.has(channel.id))];
     const invited_channels = user.invited_channels;
     const channels = {
       user_channels,
